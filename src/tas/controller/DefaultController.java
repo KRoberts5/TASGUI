1b312ea31110cd46d30405a90dbd802850cfab8b
Brendan Roberts <kroberts5@stu.jsu.edu>
2018-12-18 10:24:32 -0600

    Added MVC classes

---
+++ src/tas/controller/DefaultController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tas.controller;
+
+/**
+ *
+ * @author Brendan
+ */
+public class DefaultController extends AbstractController {
+    
+    public static final String PUNCH = "Punch";
+    public static final String EMPLOYEE = "Employee";
+    public static final String DAILY_SCHEDULE = "DailySchedule";
+    public static final String ABSENTEEISM = "Absenteeism";
+    public static final String SHIFT = "Shift";
+    public static final String TASLOGIC = "TASLogic";
+    public static final String BADGE = "Badge";
+    
+}
---
+++ src/tas/view/ViewWindow.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tas.view;
+
+/**
+ *
+ * @author Brendan
+ */
+
+import java.awt.*;
+import javax.swing.*;
+import java.beans.*;
+
+public class ViewWindow extends JFrame implements AbstractView {
+    
+    
+    
+    public void modelPropertyChange(PropertyChangeEvent e){
+        
+    }
+}
