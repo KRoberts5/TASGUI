f4590d83bfe564d411d02840c8b91e6d63e6e6cc
Brendan Roberts <kroberts5@stu.jsu.edu>
2018-12-30 09:19:23 -0600

    Organized Constants in DefaultController

--- src/tas/controller/DefaultController.java
+++ src/tas/controller/DefaultController.java
@@ -14,43 +14,71 @@ import java.util.HashMap;
  * @author Brendan
  */
 public class DefaultController extends AbstractController {
+    
+    //Badge and Employee Constants
+    public static final String ABSENTEEISM = "Absenteeism";
+    public static final String BADGE = "Badge";
     public static final String BADGE_ID = "badgeid";
-    public static final String FIRSTNAME = "firstname";
-    public static final String MIDDLENAME = "middlename";
-    public static final String LASTNAME = "lastname";
+    public static final String EMPLOYEE = "Employee";
     public static final String EMPLOYEE_TYPE_ID = "employeetypeid";
+    public static final String FIRSTNAME = "firstname";
+    public static final String LASTNAME = "lastname";
+    public static final String MIDDLENAME = "middlename";
+    
+    //Daily Schedule Constants
+    public static final String DAILY_SCHEDULE = "DailySchedule";
+    
+    //Department Constants
     public static final String DEPARTMENT_ID = "departmentid";
+    
+    //Shift Constants
+    public static final String SHIFT = "Shift";
     public static final String SHIFT_ID = "shiftid";
     
-    public static final String TERMINAL_ID = "terminalid";
-    public static final String PUNCH_TYPE_ID = "punchtypeid";
-    public static final String ORIGINAL_TIMESTAMP = "orgiginaltimestamp";
-    
-    
+    //Punch Constants
     public static final String PUNCH = "Punch";
     public static final String PUNCH_LIST = "PunchList";
-    public static final String EMPLOYEE = "Employee";
-    public static final String DAILY_SCHEDULE = "DailySchedule";
-    public static final String ABSENTEEISM = "Absenteeism";
-    public static final String SHIFT = "Shift";
+    public static final String PUNCH_TYPE_ID = "punchtypeid";
+    
+    public static final String ORIGINAL_TIMESTAMP = "orgiginaltimestamp";
+    public static final String TERMINAL_ID = "terminalid";
+    
+    //TASLogic Constants
     public static final String TASLOGIC = "TASLogic";
-    public static final String BADGE = "Badge";
-    public static final String DATE = "Date";
-    public static final String PAY_PERIOD_DATA = "PayPeriodData";
     
-    public static final String UPDATE = "Update";
+    
+    //Insert Constants
     public static final String INSERT = "Insert";
-    public static final String RETRIEVE = "Retrieve";
     
+    //Retrieve Constants
+    public static final String RETRIEVE = "Retrieve";
+
+    //Update Constants
+    public static final String UPDATE = "Update";
+    public static final String UPDATE_BADGE_IDS = "UpdateBadgeIds";
+    public static final String UPDATE_BADGE_ID = "UpdateBadgeId";
+    public static final String UPDATE_DAILY_PUNCH_LIST = "UpdateDailyPunchList";
+    public static final String UPDATE_DEP_IDS = "UpdateDepIds";
+    public static final String UPDATE_EMPLOYEE_INFO = "UpdateEmployeeInfo";
+    public static final String UPDATE_EMP_TYPE_IDS = "UpdateEmpTypeIds";
+    public static final String UPDATE_PAY_PERIOD_DATA = "UpdatePayPeriodData";
+    public static final String UPDATE_PUNCH_TYPE_IDS = "UpdatePunchTypeIds";
+    public static final String UPDATE_SHIFT_IDS = "UpdateShiftIds";
+    public static final String UPDATE_VALUES = "UpdateValues";
+    
+    //Constants for View Class Names
     public static final String LAUNCH = "Launch";
     public static final String TERMINAL = "Terminal";
     public static final String ADMIN = "Admin";
     public static final String ADMIN_CHOICE = "AdminChoice";
     public static final String INSERT_EMPLOYEE = "InsertEmployee";
+    public static final String INSERT_PUNCH = "InsertPunch";
     public static final String UPDATE_EMPLOYEE = "UpdateEmployee";
+    public static final String UPDATE_EMPLOYEE_INPUT = "UpdateEmployeeInput";
     public static final String RETRIEVE_PUNCH_LIST = "RetrievePunchList";
     public static final String RETRIEVE_PAY_PERIOD_DATA = "RetrievePayPeriodData";
     
+    //Constants for Communication with ViewTerminal
     public static final String PUNCH_IN = "PunchIn";
     public static final String PUNCH_OUT = "PunchOut";
     public static final String PUNCH_IN_FAIL_INVALID_BADGE_ID = "PunchInFailInvalidBadgeId";
@@ -58,33 +86,34 @@ public class DefaultController extends AbstractController {
     public static final String PUNCH_IN_SUCCESS = "PunchInSuccess";
     public static final String PUNCH_OUT_SUCCESS = "PunchOutSuccess";
     
+    //Constants for Setting up and Reseting GUI
     public static final String RESET_GUI = "ResetGUI";
     public static final String RETURN_HOME = "ReturnHome";
     public static final String ADMIN_GUI = "AdminGUI";
     public static final String CREATE_TOOLBAR = "CreateToolbar";
-    public static final String UPDATE_EMPLOYEE_INFO = "UpdateEmployeeInfo";
-    public static final String UPDATE_EMPLOYEE_INPUT = "UpdateEmployeeInput";
-    public static final String UPDATE_BADGE_IDS = "UpdateBadgeIds";
-    public static final String UPDATE_BADGE_ID = "UpdateBadgeId";
-    public static final String UPDATE_EMP_TYPE_IDS = "UpdateEmpTypeIds";
-    public static final String UPDATE_DEP_IDS = "UpdateDepIds";
-    public static final String UPDATE_SHIFT_IDS = "UpdateShiftIds";
-    public static final String UPDATE_DAILY_PUNCH_LIST = "UpdateDailyPunchList";
-    public static final String UPDATE_PAY_PERIOD_DATA = "UpdatePayPeriodData";
-    public static final String UPDATE_VALUES = "UpdateValues";
+    
+    //Constants for Communicating with ViewUpdateEmployee and ViewUpdateEmployeeInput
     public static final String UPDATE_EMPLOYEE_SUCCESS = "UpdateEmployeeSuccess";
     public static final String UPDATE_EMPLOYEE_FAILED = "UpdateEmployeeFailed";
-    public static final String UPDATE_PUNCH_TYPE_IDS = "UpdatePunchTypeIds";
     
+    //Constants for Communicating with ViewRetrievePunchList
     public static final String NO_DAILY_PUNCH_LIST_DATA = "NoDailyPunchListData";
-    public static final String NO_PAY_PERIOD_DATA = "NoPayPeriodData";
-    public static final String PUNCH_DATA = "PunchData";
-    public static final String PAY_PERIOD_OVERVIEW = "PayPeriodOverview";
     
-    public static final String EMPLOYEE_ENTRY = "EmployeeEntry";
-    public static final String INSERT_PUNCH = "InsertPunch";
+    //Constants for Communicating with ViewRetrievePayPeriodData
+    public static final String NO_PAY_PERIOD_DATA = "NoPayPeriodData";
+    public static final String PAY_PERIOD_DATA = "PayPeriodData";
+    public static final String PAY_PERIOD_OVERVIEW = "PayPeriodOverview";
+    public static final String PUNCH_DATA = "PunchData";
+    
+    //Constants for Communicating with ViewInsertPunch
     public static final String INSERT_PUNCH_SUCCESS = "InsertPunchSuccess";
     public static final String INSERT_PUNCH_FAILED = "InsertPunchFailed";
+    
+    //Constants for Calling Model Methods
+    public static final String EMPLOYEE_ENTRY = "EmployeeEntry";
+    
+    //Misc. Constants
+    public static final String DATE = "Date";
     
     public void returnHome(){
         showCard(ADMIN_CHOICE);
@@ -125,6 +154,4 @@ public class DefaultController extends AbstractController {
     public void insertPunch(HashMap<String,Object> values){
         insertDatabaseProperty(PUNCH,values);
     }
-    
-    
 }
